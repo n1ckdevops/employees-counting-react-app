d81b7c09346f6a6bc4f785e245dbcd4370836129
n1ckdevops <nickdevops6@gmail.com>
2024-07-15 01:48:00 +0300

    add structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-const text = "Hello world";
-const elem = (
-  <div>
-    <h2>Text: {text}</h2>
-    <input type="text" name="" id="" />
-    <button>Clock</button>
-  </div>
-);
+import App from "./components/app/app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elem);
-
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
