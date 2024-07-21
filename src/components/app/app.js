9458c36c9defc434638f7cc40c6df2f2c5998d64
n1ckdevops <nickdevops6@gmail.com>
2024-07-21 22:57:29 +0300

    Иммутабельность состояния и собственные события

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,27 +1,82 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
 import "./app.css";
 
-function App() {
-  const data = [
-    { name: "Leto J.", salary: 3000, increase: true, id: "1" },
-    { name: "Nick K.", salary: 2000, increase: false, id: "2" },
-    { name: "Stanislav K.", salary: 1231000, increase: true, id: "3" },
-  ];
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          name: "Leto J.",
+          salary: 3000,
+          increase: true,
+          id: uuidv4(),
+          like: false,
+        },
+        {
+          name: "Nick K.",
+          salary: 2000,
+          increase: false,
+          id: uuidv4(),
+          like: true,
+        },
+        {
+          name: "Stanislav K.",
+          salary: 1231000,
+          increase: true,
+          id: uuidv4(),
+          like: false,
+        },
+      ],
+    };
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: uuidv4(),
+    };
+
+    this.setState(({ data }) => {
+      const adding = [...data, newItem];
+      return {
+        data: adding,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm onAdd={this.addItem} />
       </div>
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -20,13 +20,14 @@ class EmployeesListItem extends Component {
   };
 
   render() {
-    const { name, salary } = this.props;
+    const { name, salary, onDelete } = this.props;
     const { increase, like } = this.state;
 
     let classNames = "list-group-item d-flex justify-content-between";
     if (increase) {
       classNames += " increase";
-    } else if (like) {
+    }
+    if (like) {
       classNames += " like";
     }
     return (
@@ -39,6 +40,7 @@ class EmployeesListItem extends Component {
           className="list-group-item-input"
           defaultValue={salary + "$"}
         />
+
         <div className="d-flex justify-content-center align-items-center">
           <button
             type="button"
@@ -48,7 +50,11 @@ class EmployeesListItem extends Component {
             <i className="fas fa-cookie"></i>
           </button>
 
-          <button type="button" className="btn-trash btn-sm ">
+          <button
+            type="button"
+            className="btn-trash btn-sm "
+            onClick={onDelete}
+          >
             <i className="fas fa-trash"></i>
           </button>
           <i className="fas fa-star"></i>
